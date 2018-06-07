427b442f915f0996008f238ede499733e7a4b93d
IDKiro <609568960@qq.com>
2018-06-07 15:36:09 +0800

    fix some bug

--- Registration/Demo.py
+++ Registration/Demo.py
@@ -48,31 +48,38 @@ def phacor(ImgInput1, ImgInput2):
     IR = np.fft.fftshift(np.fft.ifft2(R))
     (raw, col) = np.shape(F1)
     (ox, oy) = divmod(np.argmax(IR), col)
-    i = ox-np.floor(raw/2)-1
-    j = oy-np.floor(col/2)-1
+    i = ox-np.floor(raw/2)
+    j = oy-np.floor(col/2)
     return (i, j, IR[ox, oy])
 
 # 保持尺寸不变的图像缩放
 def imscale(ImgInput, mo, no, s):
     (mi, ni) = np.shape(ImgInput)
-    raw = round(mi/s)
-    col = round(ni/s)
     ImgScale = misc.imresize(ImgInput, 1/s)
-    if mo > raw and no > col:
-        bp = int(np.round((mo-raw)/2))
-        ap = int(mo-raw-np.round((mo-raw)/2)+1)
-        ImgOutput = np.lib.pad(ImgScale, ((bp, ap), (bp, ap)), 'constant', constant_values=0)
-    else:
-        ImgOutput = ImgScale[floor(raw/2)-floor(mo/2):floor(raw/2)-floor(mo/2)+mo, floor(col/2)-floor(no/2):floor(col/2)-floor(no/2)+no]
+    (raw, col) = np.shape(ImgScale)
+    RawBeforePad = int(np.floor((mo-raw)/2))
+    RawAfterPad = int(mo-raw-RawBeforePad)
+    ColBeforePad = int(np.floor((no-col)/2))
+    ColAfterPad = int(no-col-ColBeforePad)
+    ImgOutput = np.lib.pad(ImgScale, ((RawBeforePad, RawAfterPad), (ColBeforePad, ColAfterPad)), 'constant', constant_values=0)
     return ImgOutput
 
 if __name__=="__main__":
-    f1 = mpimg.imread('test1.jpg')
-    f2 = mpimg.imread('test2.jpg')
+    f1 = mpimg.imread('test1_origin.jpg')
+    f2 = mpimg.imread('test1_trans.jpg')
+
+    plt.subplot(131)
+    plt.imshow(f1)
+    plt.axis('off')
+    plt.subplot(132)
+    plt.imshow(f2)
+    plt.axis('off')
 
     f1g = rgb2gray(f1)
     f2g = rgb2gray(f2)
-    [raw, col] = np.shape(f1g)
+
+    
+    [raw, col] = np.shape(f1g) # TODO: 两者分辨率不一样
 
     # 图像快速傅里叶变换
     F1 = np.fft.fftshift(np.fft.fft2(f1g))
@@ -85,6 +92,13 @@ if __name__=="__main__":
     # 相位相关法求伸缩旋转量
     (loga, theta, rm) = phacor(Fr1, Fr2)
     scale = np.power(1.044, loga)
+
+    if scale < 1:
+        theta = -theta
+        scale = 1/scale
+        f1g, f2g = f2g, f1g
+        f1, f2 = f2, f1
+
 
     # 伸缩旋转变换后用相位相关法求平移量
     f2Rotate = ndimage.rotate(f2g, -theta)
@@ -101,17 +115,11 @@ if __name__=="__main__":
     # 绘制框选矩形 
     gama = np.arctan(raw/col)
     BoxHalfLen = np.power((np.power(raw/scale, 2)+np.power(col/scale, 2)), 0.5)/2
-    xOrigin = round(raw/2+Movx-BoxHalfLen*np.cos(gama+theta/180*np.pi))
-    yOrigin = round(col/2+Movy-BoxHalfLen*np.sin(gama+theta/180*np.pi))
+    xOrigin = round(raw/2+Movx-BoxHalfLen*np.cos(gama+theta/180*np.pi)-1)
+    yOrigin = round(col/2+Movy-BoxHalfLen*np.sin(gama+theta/180*np.pi)-1)
     rect = patches.Rectangle((xOrigin, yOrigin), raw/scale, col/scale, angle=theta, edgecolor='r', facecolor='none')
 
     # 绘制结果
-    plt.subplot(131)
-    plt.imshow(f1)
-    plt.axis('off')
-    plt.subplot(132)
-    plt.imshow(f2)
-    plt.axis('off')
     plt.subplot(133)
     plt.imshow(f1)
     plt.axis('off')
